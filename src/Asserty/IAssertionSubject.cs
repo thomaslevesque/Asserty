856ac1b7deac8838ca31aab02c938f5e1d237f05
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2025-04-03 15:53:05 +0200

    Hide Object members from assertion subject

--- src/Asserty/IAssertionSubject.cs
+++ src/Asserty/IAssertionSubject.cs
@@ -1,6 +1,6 @@
 ﻿namespace Asserty;
 
-public interface IAssertionSubject<out T>
+public interface IAssertionSubject<out T> : IHideObjectMembers
 {
     T Value { get; }
     string? Expression { get; }
---
+++ src/Asserty/IHideObjectMembers.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace Asserty;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+public interface IHideObjectMembers
+{
+    /// <summary>
+    /// Hides the ToString-method.
+    /// </summary>
+    /// <returns>A string representation of the implementing object.</returns>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    string? ToString();
+
+    /// <summary>
+    /// Determines whether the specified <see cref="object"/> is equal to this instance.
+    /// </summary>
+    /// <param name="o">The <see cref="object"/> to compare with this instance.</param>
+    /// <returns>
+    /// <c>true</c> if the specified <see cref="object"/> is equal to this instance; otherwise, <c>false</c>.
+    /// </returns>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    bool Equals(object? o);
+
+    /// <summary>
+    /// Returns a hash code for this instance.
+    /// </summary>
+    /// <returns>
+    /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.
+    /// </returns>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    int GetHashCode();
+
+    /// <summary>
+    /// Gets the type.
+    /// </summary>
+    /// <returns>The exact runtime type of the current instance.</returns>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    Type GetType();
+}
