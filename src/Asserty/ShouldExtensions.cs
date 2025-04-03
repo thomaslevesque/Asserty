6c8c7285a94d80c06a161680355223c650490581
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2025-04-03 14:18:01 +0200

    Ensure a valid expression is passed to Should

--- src/Asserty/ShouldExtensions.cs
+++ src/Asserty/ShouldExtensions.cs
@@ -5,8 +5,15 @@ namespace Asserty;
 
 public static class ShouldExtensions
 {
-    public static IPositiveAssertionSubject<T> Should<T>(this T value, [CallerArgumentExpression(nameof(value))] string? expression = null)
+    public static IPositiveAssertionSubject<T> Should<T>(this T value, [CallerArgumentExpression(nameof(value))] string expression = null!)
     {
+        if (string.IsNullOrWhiteSpace(expression))
+        {
+            throw new ArgumentException(
+                "In order to have meaningful assertion failure messages, expression must not be null or empty. Just leave it unspecified, the compiler will provide the value (requires C# 10 or later).",
+                nameof(expression));
+        }
+
         return new PositiveAssertionSubject<T>(value, expression);
     }
 }
---
+++ tests/Asserty.Tests/ShouldExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Asserty.Tests;
+
+public class ShouldExtensionsTests
+{
+    [Fact]
+    public void WhenPassedNoExplicitExpression_Should_ShouldReturnAnAssertionSubject()
+    {
+        const string value = "hello";
+        var subject = value.Should();
+        Assert.Equal(nameof(value), subject.Expression);
+    }
+
+    [Fact]
+    public void WhenPassedAnExplicitExpression_Should_ShouldReturnAnAssertionSubject()
+    {
+        const string value = "hello";
+        const string expression = "test";
+        var subject = value.Should(expression);
+        Assert.Equal(expression, subject.Expression);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void WhenPassedAnExplicitNullOrEmptyExpression_Should_ShouldThrow(string? expression)
+    {
+        const string value = "hello";
+        var exception = Record.Exception(() => value.Should(expression!));
+        var argumentException = Assert.IsType<ArgumentException>(exception);
+        Assert.Equal("expression", argumentException.ParamName);
+    }
+}
