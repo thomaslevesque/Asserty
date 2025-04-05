29c8787c8bcc3844359643f1ed9214f6a1a19dad
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2025-04-05 23:48:49 +0200

    Remove useless DefaultAssertion class

--- src/Asserty/Assertions/DefaultAssertion.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Asserty.Assertions;
-
-public abstract class DefaultAssertion<T> : IAssertion<T>
-{
-    public abstract bool IsVerified(T actualValue);
-    public abstract string GetExpectationDescription();
-    public abstract string GetActualDescription(T actualValue);
-    public virtual IAssertion<T> GetNegativeAssertion() => new DefaultNegativeAssertion<T>(this);
-}
