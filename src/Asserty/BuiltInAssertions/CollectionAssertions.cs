b59a3a2e2a1d8eb949a35db128555f7ac9289ac0
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2025-04-30 00:29:10 +0200

    Add HaveCount assertion

--- src/Asserty/BuiltInAssertions/CollectionAssertions.cs
+++ src/Asserty/BuiltInAssertions/CollectionAssertions.cs
@@ -27,4 +27,30 @@ public static partial class AssertionSubjectExtensions
             .DescribeActualWhenNegated(_ => "it is");
         return subject.Verify(assertion);
     }
+
+    /// <summary>
+    /// Asserts that the subject's value contains the specified number of elements.
+    /// </summary>
+    /// <param name="subject">The subject of the assertion</param>
+    /// <param name="count">The expected number of elements</param>
+    /// <typeparam name="T">The type of the assertion subject's value.</typeparam>
+    /// <returns>An assertion result that can be used to chain other assertions, if successful.</returns>
+    /// <exception cref="AssertionException">The assertion failed.</exception>
+    public static IAssertionResult<IEnumerable<T>?> HaveCount<T>(this IAssertionSubject<IEnumerable<T>?> subject, int count)
+    {
+        var assertion = AssertionBuilder.For<IEnumerable<T>?>()
+            .Verify(actualValue => actualValue is not null && actualValue.Count() == count)
+            .ExpectValue($"to contain {count} {Elements(count)}")
+            .DescribeActual(actualValue =>
+            {
+                if (actualValue is null)
+                    return "it is actually null";
+                int actualCount = actualValue.Count();
+                return $"{Format(actualValue)} contains {actualCount} {Elements(actualCount)}";
+            })
+            .DescribeActualWhenNegated(_ => "it does");
+        return subject.Verify(assertion);
+
+        static string Elements(int count) => count > 1 ? "elements" : "element";
+    }
 }
---
+++ tests/Asserty.Tests/HaveCountTests.cs
@@ -0,0 +1,58 @@
+﻿namespace Asserty.Tests;
+
+public static class HaveCountTests
+{
+    public class WhenValueHasSpecifiedCount
+    {
+        private static readonly int[] Collection = [1, 2, 3];
+
+        [Fact]
+        public void HaveCount_Should_Pass()
+        {
+            Expect(() => Collection.Should().HaveCount(3)).ToPass();
+        }
+
+        [Fact]
+        public void Not_HaveCount_Should_Fail()
+        {
+            Expect(() => Collection.Should().Not.HaveCount(3))
+                .ToFail("Expected `Collection` not to contain 3 elements, but it does.");
+        }
+    }
+
+    public class WhenValueHasDifferentCount
+    {
+        private static readonly int[] Collection = [1, 2, 3, 4];
+
+        [Fact]
+        public void HaveCount_Should_Fail()
+        {
+            Expect(() => Collection.Should().HaveCount(3))
+                .ToFail("Expected `Collection` to contain 3 elements, but [1, 2, 3, …] contains 4 elements.");
+        }
+
+        [Fact]
+        public void Not_HaveCount_Should_Pass()
+        {
+            Expect(() => Collection.Should().Not.HaveCount(3)).ToPass();
+        }
+    }
+
+    public class WhenValueIsNull
+    {
+        private static readonly int[]? Collection = null;
+
+        [Fact]
+        public void HaveCount_Should_Fail()
+        {
+            Expect(() => Collection.Should().HaveCount(3))
+                .ToFail("Expected `Collection` to contain 3 elements, but it is actually null.");
+        }
+
+        [Fact]
+        public void Not_HaveCount_Should_Pass()
+        {
+            Expect(() => Collection.Should().Not.HaveCount(3)).ToPass();
+        }
+    }
+}
